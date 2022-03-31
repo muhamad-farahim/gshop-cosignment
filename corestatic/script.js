8718006712261dddd79eab09e7ab10c004fde7b1
melly <reksbark699@gmail.com>
2022-03-31 08:13:38 +0700

    Commit pertama: template dan signup

---
+++ corestatic/script.js
@@ -0,0 +1,29 @@
+const clickforburgers = document.getElementById('clickburger')
+const dropdown = document.getElementById('dropdownid')
+const overlay = document.getElementById('overlaynav')
+
+
+clickforburgers.addEventListener('click', onClickDrop)
+overlay.addEventListener('click', onClickGONE)
+
+
+function onClickDrop() {
+
+    if (!dropdown.classList.contains('dropdownn--active')) {
+        dropdown.classList.add('dropdownn--active')
+        overlay.classList.add('overlaynav--active')
+
+    } else {
+        dropdown.classList.remove('dropdownn--active')
+        overlay.classList.remove('overlaynav--active')
+    }
+
+}
+
+function onClickGONE() {
+
+    if (dropdown.classList.contains('dropdownn--active')) {
+        dropdown.classList.remove('dropdownn--active')
+        overlay.classList.remove('overlaynav--active')
+    }
+}
